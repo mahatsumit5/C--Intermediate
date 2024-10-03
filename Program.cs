8d2fe5c873444834351c7aab2baef6210f126942
mahatsumit5 <mahatsumit5@gmail.com>
2024-10-03 19:40:45 +1000

    namespace

--- Program.cs
+++ Program.cs
@@ -1,42 +1,16 @@
 ﻿using System;
 using System.Dynamic;
+using MyApp.Models;
 
 namespace MyApp
 {
     internal class Program
     {
 
-        public class Computer 
-        {
-
-            // one way
-            // private string _motherboard; this is a field starts with lowercase and ends with ;.
-
-            // this is property it has getter and setter that returns and assigns value to the fields.
-            // private string Motherboard { get { return _motherboard; } set { _motherboard = value; }} 
-
-            // Second approach
-            // In Simple Approach the above can be written as 
-            // this will create a field name _motherboard for us and sets the value.
-            // this is a attributes it has uppercase and does not have colon ";".
-            public string Motherboard { get; set; } = "";
-
-            public int CPUCores { get; set; } 
-            public bool HasWifi { get; set; } 
-            public bool HasLTE { get; set; } 
-
-            public DateTime ReleaseDate { get; set; } 
-
-            public decimal Price { get; set; }
-
-            public string VideoCard { get; set; } = "";
-         
-     }
-
-        
+             
         static void Main(string[] args)
         {
-            Computer lenovo = new()
+            Computer lenovo = new Computer()
             { 
                 Motherboard="Lenovo",
                 Price=99.99m,
---
+++ models/Computer.cs
@@ -0,0 +1,29 @@
+namespace MyApp.Models 
+{
+  public class Computer 
+        {
+
+            // one way
+            // private string _motherboard; this is a field starts with lowercase and ends with ;.
+
+            // this is property it has getter and setter that returns and assigns value to the fields.
+            // private string Motherboard { get { return _motherboard; } set { _motherboard = value; }} 
+
+            // Second approach
+            // In Simple Approach the above can be written as 
+            // this will create a field name _motherboard for us and sets the value.
+            // this is a attributes it has uppercase and does not have colon ";".
+            public string Motherboard { get; set; } = "";
+
+            public int CPUCores { get; set; } 
+            public bool HasWifi { get; set; } 
+            public bool HasLTE { get; set; } 
+
+            public DateTime ReleaseDate { get; set; } 
+
+            public decimal Price { get; set; }
+
+            public string VideoCard { get; set; } = "";
+         
+     }
+}
